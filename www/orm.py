4a42244490133c14bc8d50f123013bbe74ce4a66
yuwenyan <yuwenyan@hotmail.com>
2016-06-08 00:22:55 -0700

    update orm

--- www/orm.py
+++ www/orm.py
@@ -52,6 +52,23 @@ def execute(sql, args): #Insert(), Update() and Delete()
 
 # ORM#############################################################
 
+class Field(object):
+    
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+    
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+    
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+        
+        
 class Model(dict, metaclass=ModelMetaclass):
 
     def __init__(self, **kw):
