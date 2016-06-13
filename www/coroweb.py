83fa2d6e6043601555a41fb41ee4e044df1aab24
yuwenyan <yuwenyan@hotmail.com>
2016-06-13 01:32:28 -0700

    update

--- www/coroweb.py
+++ www/coroweb.py
@@ -6,3 +6,21 @@ from aiohttp import web
 
 from apis import APIError
 
+def get(path):
+    '''
+    Define decorator @get('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+def post(path):
+    '''
+    Define decorator @post('/path')
+    '''
+    
