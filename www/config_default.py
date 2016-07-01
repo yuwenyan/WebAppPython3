42929886f08f931c5673eae16fce7bfc7177e949
yuwenyan <yuwenyan@hotmail.com>
2016-07-01 15:57:24 -0700

    add configuration files

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+'''
+Default configurations for developing.
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
